63644fb0603f74a65ca8f5f8f4acb93b5da61428
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-02-21 10:03:06 +0100

    Setup entities schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,15 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
+
+import { PostSchema } from './post';
+import { UserSchema } from './user';
+import { CommentSchema } from './comment';
+import { PostedBySchema } from './postedBy';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    PostSchema,
+    UserSchema,
+    CommentSchema,
+    PostedBySchema,
+  ],
 }
