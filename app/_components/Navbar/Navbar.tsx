6ce03085fcab0c6e5a4ae6a7582a756b19ea885c
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-02-27 23:17:47 +0100

    Styled the navbar logo

--- app/_components/Navbar/Navbar.tsx
+++ app/_components/Navbar/Navbar.tsx
@@ -10,10 +10,10 @@ import Logo from "../../ui/images/clean-tok-logo.png";
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
             <Link href="/">
-                <div>
-                    <Image className="cursor-pointer" src={Logo} alt="CleanTok" />
+                <div className="w-[100px] md:w-[130px]">
+                    <Image className="cursor-pointer" src={Logo} alt="CleanTok" layout="responsive" />
                 </div>
             </Link>
         </div>
