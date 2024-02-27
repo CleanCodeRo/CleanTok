26ddad63d7c8ce61851ef7a13d514dfa16615c9e
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-02-27 23:41:28 +0100

    Display sidebar display toggle

--- app/_components/Sidebar/Sidebar.tsx
+++ app/_components/Sidebar/Sidebar.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+"use client";
 
-type Props = {};
+import React, { useState } from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { GoogleLogin } from "react-google-login";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import {ImCancelCircle} from "react-icons/im";
 
-const Sidebar = (props: Props) => {
-    return <div>Sidebar</div>;
+const Sidebar = () => {
+    const [showSidebar, setShowSidebar] = useState(true);
+    const handleSidebarDisplay = () => {
+        setShowSidebar((prev) => !prev);
+    }
+
+    return (
+        <div>
+            <div className="block cursor-pointer xl:hidden m-2 ml-4 mt-3 text-xl" onClick={handleSidebarDisplay}>
+                {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
