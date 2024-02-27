3facd438310ae7cbcda308614a320bfaf16d753d
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-02-27 21:39:02 +0100

    Created basic components

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../ui/styles/globals.scss";
+import Navbar from "../components/Navbar/Navbar";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,17 @@ const RootLayout = ({
 }>) => {
     return (
         <html lang="en">
-            <body className={`${inter.className} antialiased`}>{children}</body>
+            <body className={`${inter.className} antialiased`}>
+                <div>
+                    <Navbar />
+                    <div className="flex gap-6 md:gap-20">
+                        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+                            <Sidebar />
+                        </div>
+                        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">{children}</div>
+                    </div>
+                </div>
+            </body>
         </html>
     );
 };
