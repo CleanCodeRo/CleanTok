8980a7e9fe28ab6022e0576775884d5e305a31f4
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-01 23:38:03 +0100

    Added post creation button

--- app/_components/Navbar/Navbar.tsx
+++ app/_components/Navbar/Navbar.tsx
@@ -32,7 +32,15 @@ const Navbar = () => {
 
             <div>
                 {userProfile ? (
-                    <div className="flex gap-5 md:gap-10">{userProfile.userName}</div>
+                    <div className="flex gap-5 md:gap-10">
+                        <Link href="/upload">
+                            <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                                <IoMdAdd className="text-xl" /> <span className="hidden md:block">Upload </span>
+                            </button>
+                        </Link>
+
+                        {userProfile.userName}
+                    </div>
                 ) : (
                     <GoogleLogin
                         onSuccess={(response) => createOrGetUser(response, addUser)}
