4a786c3e14ee708b9b518943f8675667ded15a82
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-01 14:00:24 +0100

    Displayed spinner while videos load

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,21 +5,26 @@ import { getPosts } from "@/app/_utils/api";
 import { Video } from "../_utils/interfaces";
 import NoResults from "../_components/NoResults/NoResults";
 import VideoCard from "../_components/VideoCard/VideoCard";
+import Spinner from "../_components/Spinner/Spinner";
 
 const HomePage = () => {
     const [videos, setVideos] = useState<Video[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const getVideosInit = async () => {
-            const result = await getPosts();
+            const result: any = await getPosts();
             setVideos(result?.data);
+            setIsLoading(false);
         };
         getVideosInit();
-    }, []);
+    }, [videos]);
 
     return (
         <div className="flex flex-col gap-10 videos h-full">
-            {videos.length ? (
+            {isLoading ? (
+                <Spinner />
+            ) : videos?.length ? (
                 videos?.map((video: Video) => <VideoCard key={video._id} post={video} />)
             ) : (
                 <NoResults text={`No Videos`} />
