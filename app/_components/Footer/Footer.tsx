050a4bcb883742d44286939321a1cff3cf72900b
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-01 02:51:13 +0100

    Display footer items

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { footerList1, footerList2, footerList3 } from "@/app/_utils/constants";
+import Link from "next/link";
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+interface FooterItemsListProps {
+    items: string[];
+    mt: boolean;
 }
 
-export default Footer
+const FooterItemsList = ({ items, mt }: FooterItemsListProps) => (
+    <div className={`flex flex-wrap gap-2 ${mt && "mt-5"}`}>
+        {items.map((item: string) => (
+            <p key={item} className="text-gray-400 text-sm hover:underline cursor-pointer">
+                {item}
+            </p>
+        ))}
+    </div>
+);
+
+const Footer = () => {
+    return (
+        <div className="mt-6 hidden xl:block">
+            <FooterItemsList items={footerList1} mt={false} />
+            <FooterItemsList items={footerList2} mt />
+            <FooterItemsList items={footerList3} mt />
+            <p className="text-gray-400 text-sm mt-5">© 2024 <Link href="https://cleancode.ro/">CleanCode.ro</Link></p>
+        </div>
+    );
+};
+
+export default Footer;
