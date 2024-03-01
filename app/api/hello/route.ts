c8d9b4f8426daa8d778f7388beb4e96e991123ac
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-01 09:56:09 +0100

    Returned api response as json

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,5 @@
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-    return new Response("Hello to the CleanTok's API!", {
-        status: 200,
-    });
+    return Response.json({ message: "Hello to the CleanTok's API!" }, { status: 200 });
 }
