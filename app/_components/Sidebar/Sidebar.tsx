47e954b2d36b250afd3fd89f7173c2f11d076264
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-01 21:04:18 +0100

    Remove Google login section from sidebar

--- app/_components/Sidebar/Sidebar.tsx
+++ app/_components/Sidebar/Sidebar.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import React, { Suspense, useState } from "react";
-import { googleClientId } from "@/app/_utils/env";
 import Link from "next/link";
-import { GoogleLogin } from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
 import Discover from "../Discover/Discover";
@@ -18,26 +16,6 @@ const Sidebar = () => {
         setShowSidebar((prev) => !prev);
     };
 
-    const handleGoogleSuccessfulAuth = () => {
-        console.log("Not yet implemented");
-    };
-
-    const handleGoogleAuthFailure = () => {
-        console.log("Not yet implemented");
-    };
-
-    const handleGoogleLoginRender = (renderProps: any) => {
-        return (
-            <button
-                className="cursor-pointer bg-white text-lg text-[#004AAD] border-[1px] border-[#004AAD] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#004AAD]"
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-            >
-                Log in
-            </button>
-        );
-    };
-
     const normalLinkClasses =
         "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#004AAD] rounded";
 
@@ -49,6 +27,7 @@ const Sidebar = () => {
             >
                 {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
             </div>
+
             {showSidebar && (
                 <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
                     <div className="xl:border-b-2 border-gray-200 xl:pb-4">
@@ -61,20 +40,6 @@ const Sidebar = () => {
                             </div>
                         </Link>
                     </div>
-                    {!userProfile && (
-                        <div className="px-2 py-4 hidden xl:block">
-                            <p className="text-gray-400">Log in to like and comment on videos</p>
-                            <div className="pr-4">
-                                <GoogleLogin
-                                    clientId={googleClientId}
-                                    render={handleGoogleLoginRender}
-                                    onSuccess={handleGoogleSuccessfulAuth}
-                                    onFailure={handleGoogleAuthFailure}
-                                    cookiePolicy="single_host_origin"
-                                />
-                            </div>
-                        </div>
-                    )}
 
                     <Suspense>
                         <Discover />
