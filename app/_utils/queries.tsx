af917cbd2c8bb5bb1cf569467075efe84940a910
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-03 00:30:24 +0100

    Added post details query

--- app/_utils/queries.tsx
+++ app/_utils/queries.tsx
@@ -28,3 +28,33 @@ export const allPostsQuery = () => {
 
     return query;
 };
+
+export const postDetailQuery = (postId: string | string[]) => {
+    const query = `*[_type == "post" && _id == '${postId}']{
+      _id,
+      caption,
+      video{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+      postedBy->{
+        _id,
+        userName,
+        profileImage
+      },
+      likes,
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          _ref,
+        },
+      }
+    }`;
+
+    return query;
+};
