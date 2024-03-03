1b21209e984a425244e219a6137d3431e7f48bc4
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-03 01:09:14 +0100

    Used profile picture component in video card

--- app/_components/VideoCard/HomeVersion.tsx
+++ app/_components/VideoCard/HomeVersion.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
-import Image from "next/image";
+import React, { useRef, useState } from "react";
 import Link from "next/link";
 import { GoVerified } from "react-icons/go";
 import { Video } from "@/app/_utils/interfaces";
+
 import PlayPauseButton from "@/app/_components/VideoCard/PlayPauseButton";
 import MuteUnmuteVideoSwitcher from "@/app/_components/VideoCard/MuteUnmuteVideoSwitcher";
+import ProfilePicture from "@/app/_components/ProfilePicture/ProfilePicture";
 
 interface IProps {
     post: Video;
@@ -22,27 +23,7 @@ const HomeVersion = ({ post }: IProps) => {
                 <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded ">
                     <div className="w-10 h-10">
                         <Link href={`/profile/${postedBy?._id}`}>
-                            <>
-                                {postedBy?.profileImage !== null ? (
-                                    <Image
-                                        width={62}
-                                        height={62}
-                                        className="rounded-full"
-                                        src={postedBy?.profileImage}
-                                        alt="user-profile"
-                                    />
-                                ) : (
-                                    <svg
-                                        className="w-full h-full text-gray-200 dark:text-gray-700 me-4"
-                                        aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-                                    </svg>
-                                )}
-                            </>
+                            <ProfilePicture profileImage={postedBy?.profileImage} />
                         </Link>
                     </div>
                     <div>
